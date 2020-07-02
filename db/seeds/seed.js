15a8699925f7849373eaa280801f69378c11b6b0
IndigoDreams88 <svidler2011@gmail.com>
2020-07-02 13:20:25 +0100

    deleted surplus console logs

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -7,15 +7,6 @@ const {
 
 const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
-// console.log(
-//   commentData.filter((comment) => {
-//     return (
-//       comment.body ===
-//       "Nihil laborum qui quidem quibusdam aut deserunt laboriosam. Enim ipsa corporis. Nobis ipsa deleniti natus nulla qui ut vero sequi. Perferendis eveniet eligendi est itaque repellat. Illum hic nesciunt omnis veniam recusandae architecto et. Cumque qui mollitia ipsam impedit nemo."
-//     );
-//   })
-// );
-
 exports.seed = function (knex) {
   return knex.migrate
     .rollback()
@@ -34,14 +25,7 @@ exports.seed = function (knex) {
     .then((articleRows) => {
       const articleRef = makeRefObj(articleRows);
       const formattedComments = formatComments(commentData, articleRef);
-      console.log(
-        formattedComments.filter((comment) => {
-          return !comment.article_id;
-          //"2017-05-06T21:33:02.394Z";
-          //2017-04-10T19:15:58.614Z
-          // 2016-06-04T18:00:17.326Z
-        })
-      );
+
       return knex("comments").insert(formattedComments);
     });
 };
