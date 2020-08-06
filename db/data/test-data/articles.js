46b934b2d0ebfabf87fb1944dc23997defb16008
IndigoDreams88 <svidler2011@gmail.com>
2020-08-06 21:11:06 +0100

    removed img urls from test and prod data

--- db/data/test-data/articles.js
+++ db/data/test-data/articles.js
@@ -6,7 +6,6 @@ module.exports = [
     body: "I find this existence challenging",
     created_at: 1542284514171,
     votes: 100,
-    imgUrl: "/Jose.jpg",
   },
   {
     title: "Sony Vaio; or, The Laptop",
@@ -15,7 +14,6 @@ module.exports = [
     body:
       "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
     created_at: 1416140514171,
-    imgUrl: "/IBM-Watson_logo.png",
   },
   {
     title: "Eight pug gifs that remind me of mitch",
@@ -23,7 +21,6 @@ module.exports = [
     author: "icellusedkars",
     body: "some gifs",
     created_at: 1289996514171,
-    imgUrl: "/cocktails.jpeg",
   },
   {
     title: "Student SUES Mitch!",
@@ -32,7 +29,6 @@ module.exports = [
     body:
       "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
     created_at: 1163852514171,
-    imgUrl: "/blueberrymuffins-12.jpg",
   },
   {
     title: "UNCOVERED: catspiracy to bring down democracy",
@@ -40,7 +36,6 @@ module.exports = [
     author: "rogersop",
     body: "Bastet walks amongst us, and the cats are taking arms!",
     created_at: 1037708514171,
-    imgUrl: "/evilCat.jpg",
   },
   {
     title: "A",
@@ -48,7 +43,6 @@ module.exports = [
     author: "icellusedkars",
     body: "Delicious tin of cat food",
     created_at: 911564514171,
-    imgUrl: "/cavemenCooking.jpg",
   },
   {
     title: "Z",
@@ -56,7 +50,6 @@ module.exports = [
     author: "icellusedkars",
     body: "I was hungry.",
     created_at: 785420514171,
-    imgUrl: "/appleCake.jpg",
   },
   {
     title: "Does Mitch predate civilisation?",
@@ -65,7 +58,6 @@ module.exports = [
     body:
       "Archaeologists have uncovered a gigantic statue from the dawn of humanity, and it has an uncanny resemblance to Mitch. Surely I am not the only person who can see this?!",
     created_at: 659276514171,
-    imgUrl: "/BarcelonaFCHistory.jpg",
   },
   {
     title: "They're not exactly dogs, are they?",
@@ -73,7 +65,6 @@ module.exports = [
     author: "butter_bridge",
     body: "Well? Think about it.",
     created_at: 533132514171,
-    imgUrl: "/fishlessFilets.jpg",
   },
   {
     title: "Seven inspirational thought leaders from Manchester UK",
@@ -81,7 +72,6 @@ module.exports = [
     author: "rogersop",
     body: "Who are we kidding, there is only one, and it's Mitch!",
     created_at: 406988514171,
-    imgUrl: "/coaches-hero.jpg",
   },
   {
     title: "Am I a cat?",
@@ -90,7 +80,6 @@ module.exports = [
     body:
       "Having run out of ideas for articles, I am staring at the wall blankly, like a cat. Does this make me a cat?",
     created_at: 280844514171,
-    imgUrl: "/256562.jpg",
   },
   {
     title: "Moustache",
@@ -98,6 +87,5 @@ module.exports = [
     author: "butter_bridge",
     body: "Have you seen the size of that thing?",
     created_at: 154700514171,
-    imgUrl: "/pumkin-center.jpg",
   },
 ];
